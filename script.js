8760ec6b4f1d08f3e7e243de77a791798996b5aa
CampbellIDS <115004817+CampbellIDS@users.noreply.github.com>
2026-02-03 22:23:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let leftPos = 0
 let bottomPos = 0
 document.getElementById('yessir').addEventListener('click', () => {
-    alert('Congratulations! You have been invited to a Valentine\'s Day dinner. Please RSVP by February 10th. We hope to see you there!');
     window.location.href = "yes.html";
 })
 document.getElementById('nooo').addEventListener('mouseover', () => {
